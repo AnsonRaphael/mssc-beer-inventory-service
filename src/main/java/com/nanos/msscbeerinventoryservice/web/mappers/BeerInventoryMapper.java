34191f4dd1b4f0e4eabfb8ddeaa32e33c887be27
AnsonRaphael <anson550220@gmail.com>
2021-12-16 01:53:45 +0530

    added initial files

---
+++ src/main/java/com/nanos/msscbeerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,12 @@
+package com.nanos.msscbeerinventoryservice.web.mappers;
+
+import com.nanos.msscbeerinventoryservice.domain.BeerInventory;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
